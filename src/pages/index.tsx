920fe8d9d983a879dfdb348397be17f144624a4a
zolooDev <zolbadrakh.dev@gmail.com>
2025-03-07 14:30:37 +0800

    debug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,7 +76,7 @@ const Home: React.FC = () => {
       </nav>
 
       {/* Movie List */}
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 mt-20 px-6">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 mt-16 px-16">
         {movies.map((movie) => (
           <div
             className="flex flex-col items-center group mt-8"
